90d371407d543bab029873c8ce53c4ba1d9b119b
kikexp <enriquepaz.inbox@gmail.com>
2018-07-28 20:41:56 -0300

    commit para deploy

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static( './dist/SisBriz/index.html' ));
+app.use(express.static( __dirname + './src' ));
 
-
-app.get('/*', function(req,res){
-	res.sendFile(path.join(__dirname, '/dist/SisBriz/index.html'));
-})
 
 app.listen(process.env.PORT || 8080,()=>{
 	console.log('console escuchando');
