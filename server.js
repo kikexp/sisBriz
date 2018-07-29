a667da11d802ba937bd4a4e3c6b4c93007d00c3d
kikexp <enriquepaz.inbox@gmail.com>
2018-07-28 22:46:05 -0300

    commit no se cuanto

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 app.use(express.static( __dirname + '/dist' ));
 
-app.listen(process.env.port || 8080);
+app.listen(process.env.PORT || 8080);
 
-/*app.get('/*', function(req, res){
+app.get('/*', function(req, res){
 	res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 
-console.log('console listening');*/
+console.log('console listening');
 
