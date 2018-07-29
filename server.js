c374181843eed23fc75a32a1baf27a5f7a71bf4e
kikexp <enriquepaz.inbox@gmail.com>
2018-07-28 21:13:14 -0300

    otro commit

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static( __dirname + './src/index.html' ));
+app.use(express.static( __dirname + '/dist' ));
 
+app.listen(process.env.port || 8080);
 
-app.listen(process.env.PORT || 8080,()=>{
-	console.log('console escuchando');
-});
+app.get('/*', function(req, res){
+	res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
+
+console.log('console listening');
 
